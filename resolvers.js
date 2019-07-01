28baf68445279bc51d1f90b7a93c0853a717382f
Martina Duemcke <mduemcke@thoughtworks.com>
2019-07-01 15:34:37 +0700

    Dealing with nested objects

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,13 @@
 const employees = [
-    { id: "1", role: "dev", yearsAtCompany: 5},
-    { id: "2", role: "QA", yearsAtCompany: 8 },
-    { id: "3", role: "dev", yearsAtCompany: 2 }
+    { id: "1", role: "dev", yearsAtCompany: 5, address: "AS15" },
+    { id: "2", role: "QA", yearsAtCompany: 8, address: "ONS50" },
+    { id: "3", role: "dev", yearsAtCompany: 2, address: "TLS55" }
+]
+
+const address = [
+    { id: "AS15", street: "Sukhumvit 15", area: "Asok" },
+    { id: "TLS55", street: "Sukhumvit 55", area: "Thonglo" },
+    { id: "ONS50", street: "Sukhumvit 50", area: "On Nut" }
 ]
 
 const Query = {
@@ -13,10 +19,19 @@ const Query = {
 const Employee = {
     id: (parent) => parent.id,
     role: (parent) => parent.role,
-    yearsAtCompany: (parent) => parent.yearsAtCompany
+    yearsAtCompany: (parent) => parent.yearsAtCompany,
+    address: (parent) => address.find(element => element.id == parent.address)
+}
+
+const Address = {
+    id: (parent) => parent.id,
+    street: (parent) => parent.street,
+    area: (parent) => parent.area
 }
 
 
 module.exports = {
-    Query
+    Query,
+    Employee,
+    Address
 }
