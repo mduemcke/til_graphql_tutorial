5940e82c54390b5de9949f0251bdc476c7dfb484
Martina Duemcke <mduemcke@thoughtworks.com>
2019-07-01 15:25:00 +0700

    Adding a query that returns a type object

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,18 @@
+const employees = [
+    { id: "1", role: "dev", yearsAtCompany: 5},
+    { id: "2", role: "QA", yearsAtCompany: 8 },
+    { id: "3", role: "dev", yearsAtCompany: 2 }
+]
+
 const Query = {
-    hallo: () => "Halli und hallo"
+    hallo: () => "Halli und hallo",
+    listEmployees: () => employees
+}
+
+const Employee = {
+    id: (parent) => parent.id,
+    role: (parent) => parent.role,
+    yearsAtCompany: (parent) => parent.yearsAtCompany
 }
 
 
